66e2670a2b8643bdb0363f92caf0986e6b69281b
Jared Allen <jaredlallen76@gmail.com>
2021-02-15 17:47:19 -0600

    adding directory locals for easier synchronization

---
+++ allengine-update/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil .
+      (
+       (relative-compile-command . (concat "pdflatex --synctex=1 ~/jared-all-dev/allengine-update/allengine-update.tex && "
+                                           "pdflatex --synctex=1 ~/jared-all-dev/allengine-update/allengine-update.tex" ) )
+       (pdf-location . "~/jared-all-dev/allengine-update/allengine-update.pdf")
+       )))
